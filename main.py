db3e543305ba7c745793fe1f7f9e19a510e6cdf0
Muhammad Ali Urakov <106965847+Urakov-Ali@users.noreply.github.com>
2022-08-21 21:44:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import logging
-#kanalga ulanganini tekshirish uchun buttons faylini import qilamiz
+#file buttons is imported to check if the user has been subscribed to our channel
 import buttons as key
-#config faylda hamma dannila bor [channel, admin, chat_id] lar
+#all the data is in the config file [channel, admin, chat_id] lar
 import config as cfg
-#asyncio bizga habar o'chishidan oldin 5 soniya kutish uchun kerak
+#wee need asyncio to wait for 5 seconds before the message is deleted
 import asyncio
-#until_date ni hisoblash uchun datetime ni import qilamiz until_date = \
+#datetime is imported to calculate the until_date modul until_date = \
 #datetime.datetime.now() + datetime.timedelta(minutes=int(message.text[6:]))
 import datetime
  
@@ -16,32 +16,31 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=cfg.TOKEN)
 dp = Dispatcher(bot)
 
-#1) bot foydalanuvchi kanalga qo'shilgan qo'shilmaganini tekshiradi
-#2) bot yangi kirgan foydalanuchini username sini olib kanalga qo'shilishni so'raydi
+#1) the bot checks if the user has been subscribed to our channel or not
+#2) bot takes the username of user and asks him/her to join the channel
 @dp.message_handler(content_types=["new_chat_members"])
 async def new_comer(message: types.Message):
 	await message.reply(f"Hi 👋 {message.from_user.username} \nWelcome to the group\
 		\n to be member of our group please join our channel first !!!", reply_markup=key.channel)
 
-#foydalanuvchi kanalga qo'shilgan qo'shilmaganini tekshiruvchi funksiya yasavolamiz
-#chat_member bizda (await bot.get_chat_member(chat_id= cfg.CHANNEL_ID, user_id= message.from_user.id)) \
-#qiymatini oladi
+#we make a function to check if the user has been subscribed to our channel or not
+#chat_member takes the value of(await bot.get_chat_member(chat_id= cfg.CHANNEL_ID, user_id= message.from_user.id))
 def check_sub_channel(chat_member):
 	return chat_member['status'] != "left"
 
-#foydalanuchini malum muddatga ban qilib qo'yuvchi funksiya
+#function to ban the user for certain time
 @dp.message_handler(commands=['mute'], commands_prefix= "/")
 async def muted(message: types.Message):
-	#buyruqnu admin berganligini tekshiramiz
+	#we check if the order is given by admin
 	if str(message.from_user.id) == cfg.ADMIN_ID:
-		#agar admin buyruqni foydalanuchi habariga reply qilmasa pasdagi yozuv chiqadi
+		#if the admin doesn't make a reply to the message of the user, then appears those words
 		if not message.reply_to_message:
 			await message.reply("This should be replied command")
 		else:
-		#until_date yasab olamz / int(message.text[6:]) bu /unmute 10 yani buyruqdagi raqamlarni teruvchi qismi
+		#lets's make until_date / int(message.text[6:]) bu /unmute 10
 			until_date = datetime.datetime.now() + datetime.timedelta(minutes=int(message.text[6:]))
-			#foydalanuvchini malum muddatga bloklaymiz / [message.reply_to_message.from_user.id] bu biz so'zini\
-			#reply qilgan foydalanuvhimizning id si
+			#the user is blocked for a certain time / [message.reply_to_message.from_user.id] bu biz so'zini\
+			#id of the user that we made a reply to his/her message
 			await message.chat.restrict(user_id=message.reply_to_message.from_user.id, can_send_messages=False, until_date=until_date)
 			await message.reply(f'Dear {message.reply_to_message.from_user.username}\
 			 \nthe admin banned you for {message.text[6:]} minutes for sending banned words')
@@ -49,12 +48,11 @@ async def muted(message: types.Message):
 			await message.reply("Please don't use banned words \nYour message will be deleted in 5 seconds")
 			await asyncio.sleep(5)
 			await message.reply_to_message.delete()
-#qayta yozishga ruhsat berish buyrug'i
+#order to let the user write again
 @dp.message_handler(commands=['unmute'], commands_prefix= "/")
 async def mutet(message: types.Message):
 	await message.chat.restrict(user_id=message.reply_to_message.from_user.id, can_send_messages=True)
 
-#start bosilganda bot foydalnuvchi kanalga ulanganmi yoqmi tekshiradi
 @dp.message_handler(commands= ['start'], commands_prefix ="/")
 async def checker(message: types.Message):
 	if check_sub_channel(await bot.get_chat_member(chat_id= cfg.CHANNEL_ID, user_id= message.from_user.id)):
@@ -63,7 +61,7 @@ async def checker(message: types.Message):
 		await message.answer(f"Dear {message.from_user.username}\
 		 \nto use the bot, please subscribe our channel first !!!", reply_markup=key.channel)
 
-#taqiqlangan so'z bor yoqligini tekshiruvchi handler
+#handler to check if the banned word is written on group
 @dp.message_handler()
 async def ask_to_join_channel(message: types.Message):
 	if check_sub_channel(await bot.get_chat_member(chat_id= cfg.CHANNEL_ID, user_id= message.from_user.id)):
@@ -78,7 +76,7 @@ async def ask_to_join_channel(message: types.Message):
 		 \nto use the bot, please subscribe our channel first !!!", reply_markup=key.channel)
 
 
-#[#test] buyrugini yozish orqali oz id ingizni biling
+#[#test] know your id by sending this order
 @dp.message_handler(commands=["test"], commands_prefix="#")
 async def testing(message: types.Message):
 	if check_sub_channel(await bot.get_chat_member(chat_id = cfg.CHANNEL_ID, user_id= message.from_user.id)):
